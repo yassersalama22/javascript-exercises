80c01b225c7d3a6641ec828fea70ac3651646305
Yasser Salama <yasser.salama@gmail.com>
2023-06-09 11:07:23 +0300

    02 Soltion

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(inputString,num) {
+    let resultString = '';
+    if(num<0){
+        return 'ERROR';
+    }
+    for (let index = 0; index < num; index++) {
+        resultString+=inputString;
+    }
+    return resultString;
 };
 
 // Do not edit below this line
