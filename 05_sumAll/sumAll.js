a7143d6bce32af899b9f99737bdf552b258d9e89
Yasser Salama <yasser.salama@gmail.com>
2023-06-11 15:28:15 +0300

    05 Solution Initial

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,25 @@
-const sumAll = function() {
+const sumAll = function(number1, number2) {
+    // check number 1 is number and greater than or equal 0
+    if(typeof(number1)!="number" || number1 < 0)
+    {
+        return "ERROR";
+    }
+    // check number 2 is number and greater than or equal 0
+    if(typeof(number2)!="number" || number1 < 0)
+    {
+        return "ERROR";
+    }
+
+    // now sort the arguments to start from the smaller number
+    arguments = arguments.sort();
+    // the varaiable will hold the value of the sum
+    let sum = 0;
+    // loop through the range of the argument and sum the numbers
+    for (let index = arguments[0]; index <= arguments[1]; index++) {
+        sum += index;
+    }
+
+    return sum;
 
 };
 
