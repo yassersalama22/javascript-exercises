68cff5094aaf82d181e0fca25d77232b9c5dafe4
Yasser Salama <yasser.salama@gmail.com>
2023-06-12 15:01:02 +0300

    09 Solutions

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,8 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    let regx = /[^A-Za-z0-9]/g;
+    string  = string.toLowerCase().replace(regx,"");
+    let stringToCompare = string.split("").reverse().join("");
+    return (string === stringToCompare);
 };
 
 // Do not edit below this line
