58f428513478834c2e0b408bf89bc71886a4f306
Yasser Salama <yasser.salama@gmail.com>
2023-06-13 13:58:27 +0300

    12 Solution

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,24 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    const reducer = (p1,p2) => {
+        const currentYear = new Date().getFullYear();
+        if(p1.yearOfDeath === undefined){
+            p1.yearOfDeath = currentYear;
+        }
 
+        if(p2.yearOfDeath === undefined){
+            p2.yearOfDeath = currentYear;
+        }
+
+        if((p1.yearOfDeath-p1.yearOfBirth) > (p2.yearOfDeath-p2.yearOfBirth)) {
+            return p1;
+        }
+        else
+        {
+            return p2;
+        }
+
+    };
+    return people.reduce(reducer);
 };
 
 // Do not edit below this line
