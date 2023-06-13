d0187afaf1194901c476f9884c5ba3b2d1bec036
Yasser Salama <yasser.salama@gmail.com>
2023-06-13 11:54:10 +0300

    10 Solution

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,20 @@
-const fibonacci = function() {
-
+const fibonacci = function(number) {
+    let init = 0;
+    let sum = 0;
+    number = typeof(number) === "number" ? number : parseInt(number);
+    let febseq = [0,1];
+    if(typeof(number) === "number" && number > 0)
+    {
+        for (let index = 1; index <= number; index++) {
+            
+            febseq.push(febseq[index]+febseq[index-1]);
+        }
+        return febseq[number];
+    }
+    else
+    {
+        return "OOPS";
+    }
 };
 
 // Do not edit below this line
