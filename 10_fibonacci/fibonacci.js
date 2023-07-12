75b16ca9dccdeed90a6a373a9d2c0b3e29823cfd
Yasser Salama <yasser.salama@gmail.com>
2023-07-12 11:41:00 +0300

    solve fib

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,17 @@
-const fibonacci = function() {
-
+const fibonacci = function(number) {
+    if(number > 0) {
+    let fib = [0,1];
+    for (let index = 2; index <= number; index++) {
+        console.log(fib[index-2],fib[index-1]);
+        fib.push( fib[index-2] + fib[index-1]);
+    }
+    console.table(fib);
+    return Number(fib.slice(-1));
+}
+else {
+    return "OOPS";
+}
+    
 };
 
 // Do not edit below this line
